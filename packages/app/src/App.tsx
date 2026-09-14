71213f4921be73dd479c8b9571b07b2b789f527c
Paimon Sorornejad <paimon.soror@gmail.com>
2026-09-13 20:57:57 -0400

    courier: request template, team groups and OAuthClient status
    Enable the scaffolder (Create) and org (group pages) frontend plugins. Load Courier team groups, the OAuth client request template and per-client Resource entities from github.com/paimonsoror/courier. Show courier.sororlab.dev OAuthClients as Kubernetes custom resources.

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -2,6 +2,8 @@ import { createApp } from '@backstage/frontend-defaults';
 import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import kubernetesPlugin from '@backstage/plugin-kubernetes/alpha';
 import argoCDPlugin from '@roadiehq/backstage-plugin-argo-cd/alpha';
+import scaffolderPlugin from '@backstage/plugin-scaffolder/alpha';
+import orgPlugin from '@backstage/plugin-org/alpha';
 import { navModule } from './modules/nav';
 import { homeModule } from './modules/home';
 
@@ -77,6 +79,8 @@ export default createApp({
     catalogPlugin,
     kubernetesPlugin,
     argoCDPlugin,
+    scaffolderPlugin,
+    orgPlugin,
     navModule,
     homeModule,
     authModule,
